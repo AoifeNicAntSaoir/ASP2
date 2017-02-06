5c75b1b89096b651306ffb8a634529fa11cb29c5
Aoife Sayers <aoifesayers@gmail.com>
2017-02-06 14:50:51 +0000

    initial commit

---
+++ ASP2/TestClass.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace ASP2
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [TestCase]
+        public void add()
+        {
+            int expected = 10;
+
+            ASP2.function f = new ASP2.function();
+
+            Assert.AreEqual(f.add(5,5), expected);
+        }
+
+        
+    }
+}
