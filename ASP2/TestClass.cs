474cea27a19ac37678d9401284734269ddf9f079
Aoife <aoifesayers@gmail.com>
2017-02-09 09:13:29 +0000

    Fixed failing method

--- ASP2/TestClass.cs
+++ ASP2/TestClass.cs
@@ -18,7 +18,7 @@ namespace ASP2
         [TestCase]
         public void testSubtract()
         {
-            int expected = 50;
+            int expected = 10;
 
             ASP2.Function f = new ASP2.Function();
 
