90cb7bebb1bc17a911b8e5cac819e27132afc88f
Aoife <aoifesayers@gmail.com>
2017-02-24 09:28:54 +0000

    Pre demo changes

--- ASP2/TestClass.cs
+++ ASP2/TestClass.cs
@@ -2,25 +2,21 @@
 
 namespace ASP2
 {
+    //Class that carries out NUnit Tests on Function.cs
     [TestFixture]
     public class TestClass
     {
         [TestCase]
         public void add()
         {
-            int expected = 10;
             ASP2.Function f = new ASP2.Function();
-            Assert.AreEqual(f.add(5,5), expected);
+            Assert.AreEqual(f.add(5,5), 10);
         }
-
         [TestCase]
         public void testSubtract()
         {
-            int expected = 10;
             ASP2.Function f = new ASP2.Function();
-            Assert.AreEqual(f.subtract(20, 10), expected);
+            Assert.AreEqual(f.subtract(20, 10), 10);
         }
-
-
     }
 }
--- ASP2/function.cs
+++ ASP2/function.cs
@@ -5,14 +5,13 @@ using System.Web;
 
 namespace ASP2
 {
+    //Contains add() & subtract() methods that are Unit tested by NUnit in TestClass.cs
     public class Function
     {
-
         public int add(int a, int b)
         {
             return a + b;
         }
-
         public int subtract(int a, int b)
         {
             return a - b;
